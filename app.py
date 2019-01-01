5dbd35e2f11199f232abe514c404d471adfdf2ea
Dad 2 JRN <dad2jrn@gmail.com>
2018-12-31 21:49:51 -0500

    conditional

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def method_name():
-   city = 'London'
+   if request.method == 'POST':
+      city = request.form.get('city')
+   else:
+      city = 'Los Angeles'
    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid=216526f7de265c4a32d3666c4012324f'
    response = requests.get(url).json()
-   # print(response)
-
    weather = {
       'city': city.capitalize(),
       'temp': int(response['main']['temp']),
       'description': response['weather'][0]['description'].capitalize(),
       'icon': response['weather'][0]['icon']
    }
-
-   # print(weather)
-
    return render_template('weather.html', weather=weather)
 
 
