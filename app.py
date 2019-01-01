d3502f9fa64abd4b29fe6c817fb9cc2d5cc186bc
Dad 2 JRN <dad2jrn@gmail.com>
2018-12-31 21:19:18 -0500

    formatting and styling

--- app.py
+++ app.py
@@ -7,11 +7,21 @@ app.debug = True
 
 @app.route('/')
 def method_name():
-   city = 'las vegas,us'
+   city = 'London'
    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid=216526f7de265c4a32d3666c4012324f'
    response = requests.get(url).json()
-   print(response)
-   return render_template('weather.html')
+   # print(response)
+
+   weather = {
+      'city': city.capitalize(),
+      'temp': int(response['main']['temp']),
+      'description': response['weather'][0]['description'].capitalize(),
+      'icon': response['weather'][0]['icon']
+   }
+
+   # print(weather)
+
+   return render_template('weather.html', weather=weather)
 
 
 if __name__ == '__main__':
